646d999e9b954a7511be9836fd9139da19e36a2e
Safarov Arthur <ttttwobata@gmail.com>
2025-12-24 09:23:56 +0500

    Update student_solution.py

--- student_solution.py
+++ student_solution.py
@@ -2,44 +2,57 @@
 
 # ---------- ЗАДАНИЕ 1 ----------
 def task1(s):
-    # s — строка вида "подстрока1,подстрока2"
-    # вернуть кортеж: (len(sub1) > len(sub2), sub1==sub2, sub2 in sub1)
-    ...
+    s=input()
+first, second=s.split(',')
+print(len(first)>len(second))
+print(first==second)
+print(second in first)
 
 # ---------- ЗАДАНИЕ 2 ----------
 def task2(s):
-    # s — любая строка
-    # вернуть кортеж:
-    # (s.strip(), len(s), s.count('a'), s.replace('a','@'), s.istitle())
-    ...
+    s=input()
+stripped=s.strip()
+print(stripped)
+print(len(s))
+print(s.count('a'))
+print(s.replace('a', '@'))
+print(s.istitle())
+
 
 # ---------- ЗАДАНИЕ 3 ----------
 def task3(s):
-    # s — строка
-    # вернуть кортеж: (без первого и последнего символа, каждый второй символ, строка.lower() в обратном порядке)
-    ...
+    s=input()
+result1=s[1:-1]
+print(result1)
+result2=s[::2]
+print(result2)
+result3=s[::-1].lower()
+print(result3)
 
 # ---------- ЗАДАНИЕ 4 ----------
 def task4(nums):
-    # nums — список чисел
-    # вернуть кортеж: (отсортированный список, сумма, (min, max))
-    ...
+    numbers=list(map(int, input().split()))
+print(sum(numbers))
+print(min(numbers), max(numbers))
 
 
 # ---------- ЗАДАНИЕ 5 ----------
 def task6(s):
-    # s — строка
-    # вернуть True если палиндром (без учёта регистра) и нет пробелов, иначе False
-    ...
+    s=input()
+print(s.lower()==s.lower()[::-1] and ' ' not in s)
 
 # ---------- ЗАДАНИЕ 6 ----------
 def task7(n):
-    # n — целое число
-    # вернуть кортеж: (hex(n) без '0x', len(hex), True если 'a' есть в hex)
-    ...
+    s=input()
+print(s.lower()==s.lower()[::-1] and ' ' not in s)
+n=int(input())
+h=hex(n)[2:]
+print(h)
+print(len(h))
+print('a' in h)
+
 
 # ---------- ЗАДАНИЕ 7 ----------
 def task8(month_num):
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    # вернуть название месяца по номеру (1-12)
-    ...
+    months=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+print(months[int(input()) - 1])
